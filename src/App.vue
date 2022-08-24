29c7ab3454639a3fc46aa2dcba7cd18925fdf463
xiaziwei <xiaziwei@megvii.com>
2022-08-24 13:40:23 +0800

    feat:增加乐谱

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
-<script setup></script>
-
 <template>
+  <div class="line">
+    <div class="start"></div>
+    <div ref="music" class="music">
+      <div
+        v-for="(item, index) in music"
+        :key="index"
+        :style="{ marginLeft: item.left + 'px' }"
+        class="content"
+      >
+        {{ item.key }}
+      </div>
+    </div>
+  </div>
+
   <div className="boxs">
     <div
       v-for="item in voiceArr"
@@ -37,6 +49,56 @@ export default {
         { key: "K", value: "tom" },
         { key: "L", value: "tink" },
       ],
+      music: [
+        {
+          key: "A",
+          left: 0,
+        },
+        {
+          key: "S",
+          left: 50,
+        },
+        {
+          key: "J",
+          left: 30,
+        },
+        {
+          key: "D",
+          left: 80,
+        },
+        {
+          key: "A",
+          left: 0,
+        },
+        {
+          key: "S",
+          left: 50,
+        },
+        {
+          key: "J",
+          left: 30,
+        },
+        {
+          key: "D",
+          left: 80,
+        },
+        {
+          key: "A",
+          left: 0,
+        },
+        {
+          key: "S",
+          left: 50,
+        },
+        {
+          key: "J",
+          left: 30,
+        },
+        {
+          key: "D",
+          left: 80,
+        },
+      ],
     };
   },
   methods: {
@@ -50,6 +112,12 @@ export default {
   },
   mounted() {
     window.addEventListener("keydown", this.palySound);
+    const music = this.$refs.music;
+    let left = 0;
+    setInterval(() => {
+      music.style.left = left + "px";
+      left = left - 3;
+    }, 16);
   },
 };
 </script>
@@ -57,6 +125,38 @@ export default {
 <style scoped>
 .boxs {
   display: flex;
+}
+.line {
+  width: 800px;
+  height: 88px;
+  background-color: aquamarine;
+  margin-bottom: 180px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+.start {
+  height: 100%;
+  width: 1px;
+  margin-left: 100px;
+  background-color: red;
+}
+.music {
+  width: 100000%;
+  height: 50%;
+  background-color: #999;
+  position: absolute;
+  display: flex;
+  justify-content: flex-start;
+}
+.content {
+  display: inline-block;
+  height: 100%;
+  width: 50px;
+  line-height: 50px;
+  background-color: #fff;
+  color: #000;
 }
 .keybox {
   width: 80px;
